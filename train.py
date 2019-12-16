697e2ee9d58b1516680d46633730d1b5508cac24
chizhanyuefeng <chizhanyuefeng@gmail,com>
2019-12-16 12:54:15 +0800

    update train

--- train.py
+++ train.py
@@ -10,19 +10,13 @@ from db_config import cfg
 
 import lib.networks.model as model
 from lib.networks.losses import compute_loss
-import lib.dataset.dataload as dataload
+from lib.dataset.dataloader import get_batch
 
 import warnings
 warnings.filterwarnings("ignore")
 
-
-gpus = cfg.TRAIN.GPU_LIST.split(',')
-
-
-
-
 def tower_loss(images, gt_score_maps, gt_threshold_map, gt_score_mask,
-               gt_thresh_mask, reuse_variables=None):
+               gt_thresh_mask, reuse_variables):
 
     with tf.variable_scope(tf.get_variable_scope(), reuse=reuse_variables):
         binarize_map, threshold_map, thresh_binary = model.model(images, is_training=True)
@@ -34,12 +28,16 @@ def tower_loss(images, gt_score_maps, gt_threshold_map, gt_score_mask,
 
     # add summary
     if reuse_variables is None:
-        tf.summary.image('input', images)
-        tf.summary.image('score_map', score_maps)
-        tf.summary.image('score_map_pred', f_score * 255)
-        tf.summary.image('geo_map_0', geo_maps[:, :, :, 0:1])
-        tf.summary.image('geo_map_0_pred', f_geometry[:, :, :, 0:1])
-        tf.summary.image('training_masks', training_masks)
+        tf.summary.image('gt/input_imgs', images)
+        tf.summary.image('gt/score_map', gt_score_maps)
+        tf.summary.image('gt/threshold_map', gt_threshold_map * 255)
+        tf.summary.image('gt/score_mask', gt_score_mask)
+        tf.summary.image('gt/thresh_mask', gt_thresh_mask)
+
+        tf.summary.image('pred/binarize_map', binarize_map)
+        tf.summary.image('pred/threshold_map', threshold_map * 255)
+        tf.summary.image('pred/thresh_binary', thresh_binary)
+
         tf.summary.scalar('model_loss', model_loss)
         tf.summary.scalar('total_loss', total_loss)
 
@@ -88,4 +86,145 @@ def _train_logger_init():
     train_logger.addHandler(consol_handler)
     return train_logger
 
+
+def main():
+    import os
+    os.environ['CUDA_VISIBLE_DEVICES'] = cfg.TRAIN.VIS_GPU
+    if not tf.gfile.Exists(cfg["TRAIN"]["CHECKPOINTS_OUTPUT_DIR"]):
+        tf.gfile.MkDir(cfg["TRAIN"]["CHECKPOINTS_OUTPUT_DIR"])
+
+    train_logger = _train_logger_init()
+
+    input_images = tf.placeholder(tf.float32, shape=[None, None, None, 3], name='input_images')
+    input_score_maps = tf.placeholder(tf.float32, shape=[None, None, None, 1], name='input_score_maps')
+    input_threshold_maps = tf.placeholder(tf.float32, shape=[None, None, None, 1], name='input_threshold_maps')
+
+    input_score_masks = tf.placeholder(tf.float32, shape=[None, None, None, 1], name='input_score_masks')
+    input_threshold_masks = tf.placeholder(tf.float32, shape=[None, None, None, 1], name='input_threshold_masks')
+
+    global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
+
+    learning_rate = tf.train.exponential_decay(cfg["TRAIN"]["LEARNING_RATE"], global_step, decay_steps=10000,
+                                               decay_rate=0.94, staircase=True)
+
+    if cfg.TRAIN.OPT == 'adam':
+        # learning_rate = tf.constant(cfg["TRAIN"]["LEARNING_RATE"], tf.float32)
+        opt = tf.train.AdamOptimizer(learning_rate)
+    elif cfg.TRAIN.OPT == 'momentum':
+        opt = tf.train.MomentumOptimizer(learning_rate, 0.9)
+    else:
+        assert 0, 'error optimzer'
+    print('use ', cfg.TRAIN.OPT)
+
+    # add summary
+    tf.summary.scalar('learning_rate', learning_rate)
+
+    gpus = cfg.TRAIN.VIS_GPU.split(',')
+    input_images_split = tf.split(input_images, len(gpus))
+    input_score_maps_split = tf.split(input_score_maps, len(gpus))
+    input_threshold_maps_split = tf.split(input_threshold_maps, len(gpus))
+    input_score_masks_split = tf.split(input_score_masks, len(gpus))
+    input_threshold_masks_split = tf.split(input_threshold_masks, len(gpus))
+
+
+    tower_grads = []
+    reuse_variables = None
+    for i, gpu_id in enumerate(gpus):
+        print('gpu_id', gpu_id)
+        with tf.device('/gpu:' + gpu_id):
+            with tf.name_scope('model_' + gpu_id) as scope:
+                gt_imgs = input_images_split[i]
+                gt_scores = input_score_maps_split[i]
+                gt_thresholds = input_threshold_maps_split[i]
+                gt_score_masks = input_score_masks_split[i]
+                gt_threshold_masks = input_threshold_masks_split[i]
+                total_loss, model_loss = tower_loss(gt_imgs, gt_scores, gt_thresholds, gt_score_masks, gt_threshold_masks, reuse_variables)
+                reuse_variables = True
+
+                batch_norm_updates_op = tf.group(*tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope))
+
+                grads = opt.compute_gradients(total_loss)
+                tower_grads.append(grads)
+
+    grads = average_gradients(tower_grads)
+    apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
+
+    summary_op = tf.summary.merge_all()
+
+    variable_averages = tf.train.ExponentialMovingAverage(cfg["TRAIN"]["MOVING_AVERAGE_DECAY"], global_step)
+
+    variables_averages_op = variable_averages.apply(tf.trainable_variables())
+
+    with tf.control_dependencies([variables_averages_op, apply_gradient_op, batch_norm_updates_op]):
+        train_op = tf.no_op(name='train_op')
+
+    saver = tf.train.Saver(tf.global_variables(), max_to_keep=cfg.TRAIN.SAVE_MAX)
+
+    if not tf.gfile.Exists(cfg["TRAIN"]["TRAIN_LOGS"]):
+        tf.gfile.MkDir(cfg["TRAIN"]["TRAIN_LOGS"])
+    summary_writer = tf.summary.FileWriter(cfg["TRAIN"]["TRAIN_LOGS"], tf.get_default_graph())
+
+    init = tf.global_variables_initializer()
+
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
+        try:
+
+            if cfg["TRAIN"]["RESTORE"]:
+                train_logger.info('continue training from previous checkpoint')
+                ckpt = tf.train.get_checkpoint_state(cfg["TRAIN"]["RESTORE_CKPT_PATH"])
+                train_logger.info('restore model path:', ckpt.model_checkpoint_path)
+                saver.restore(sess, ckpt.model_checkpoint_path)
+                train_logger.info("done")
+            elif cfg["TRAIN"]["PRETRAINED_MODEL_PATH"] is not None:
+                sess.run(init)
+                print(cfg["TRAIN"]["PRETRAINED_MODEL_PATH"])
+                train_logger.info('load pretrain model:{}', str(cfg["TRAIN"]["PRETRAINED_MODEL_PATH"]))
+                variable_restore_op = slim.assign_from_checkpoint_fn(cfg["TRAIN"]["PRETRAINED_MODEL_PATH"],
+                                                                     slim.get_trainable_variables(),
+                                                                     ignore_missing_vars=True)
+                variable_restore_op(sess)
+                train_logger.info("done")
+
+            else:
+                sess.run(init)
+        except:
+            assert 0, 'load error'
+
+        data_generator = get_batch(num_workers=cfg["TRAIN"]["NUM_READERS"],
+                                   batch_size=cfg["TRAIN"]["BATCH_SIZE_PER_GPU"] * len(gpus))
+
+        start = time.time()
+        for step in range(cfg["TRAIN"]["MAX_STEPS"]):
+            data = next(data_generator)
+
+            feed_dict = {input_images: data[0],
+                         input_score_maps: data[1],
+                         input_threshold_maps: data[3],
+                         input_score_masks: data[2],
+                         input_threshold_masks: data[4]}
+
+            ml, tl, _ = sess.run([model_loss, total_loss, train_op], feed_dict=feed_dict)
+            if np.isnan(tl):
+                train_logger.info('Loss diverged, stop training')
+                break
+
+            if step % 10 == 0:
+                avg_time_per_step = (time.time() - start) / 10
+                avg_examples_per_second = (10 * cfg["TRAIN"]["BATCH_SIZE_PER_GPU"] * len(gpus)) / (time.time() - start)
+                start = time.time()
+                train_logger.info(
+                    '{}->Step {:06d}, model loss {:.4f}, total loss {:.4f}, {:.2f} seconds/step, {:.2f} examples/second'.format(
+                        cfg.TRAIN.VERSION, step, ml, tl, avg_time_per_step, avg_examples_per_second))
+
+            if step % cfg["TRAIN"]["SAVE_CHECKPOINT_STEPS"] == 0:
+                saver.save(sess, os.path.join(cfg["TRAIN"]["CHECKPOINTS_OUTPUT_DIR"], cfg.TRAIN.VERSION + '_DB_model.ckpt'), global_step=global_step)
+
+            if step % cfg["TRAIN"]["SAVE_SUMMARY_STEPS"] == 0:
+                _, tl, summary_str = sess.run([train_op, total_loss, summary_op], feed_dict=feed_dict)
+                summary_writer.add_summary(summary_str, global_step=step)
+
+
+if __name__ == '__main__':
+
+    main()
 
