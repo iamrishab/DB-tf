683e54e400ca14c9c0dbbdaa0ee24744aadec860
chizhanyuefeng <chizhanyuefeng@gmail,com>
2019-12-16 10:39:08 +0800

    add total loss and fix smooth l1 loss bugs

--- lib/networks/losses.py
+++ lib/networks/losses.py
@@ -43,75 +43,34 @@ def softmax_cross_entropy(y_true_cls, y_pred_cls,
     return cls_loss
 
 
-def _smooth_l1_loss_base(bbox_pred, bbox_targets, sigma=1.0):
+def smooth_l1_loss(pred, gt, mask, sigma=1.0):
     '''
 
-    :param bbox_pred: [-1, 4] in RPN. [-1, cls_num+1, 4] or [-1, cls_num+1, 5] in Fast-rcnn
+    :param bbox_pred:
     :param bbox_targets: shape is same as bbox_pred
     :param sigma:
     :return:
     '''
     sigma_2 = sigma**2
 
-    box_diff = bbox_pred - bbox_targets
+    diff = pred * mask - gt
 
-    abs_box_diff = tf.abs(box_diff)
+    abs_diff = tf.abs(diff)
 
     smoothL1_sign = tf.stop_gradient(
-        tf.to_float(tf.less(abs_box_diff, 1. / sigma_2)))
-    loss_box = tf.pow(box_diff, 2) * (sigma_2 / 2.0) * smoothL1_sign \
-               + (abs_box_diff - (0.5 / sigma_2)) * (1.0 - smoothL1_sign)
-    return loss_box
+        tf.to_float(tf.less(abs_diff, 1. / sigma_2)))
+    loss = tf.pow(diff, 2) * (sigma_2 / 2.0) * smoothL1_sign \
+               + (abs_diff - (0.5 / sigma_2)) * (1.0 - smoothL1_sign)
+    return loss
 
 
-def total_loss(y_true_cls, y_pred_cls,
-         y_true_geo, y_pred_geo,
-         training_mask):
-    '''
-    define the loss used for training, contraning two part,
-    the first part we use dice loss instead of weighted logloss,
-    the second part is the iou loss defined in the paper
-    :param y_true_cls: ground truth of text
-    :param y_pred_cls: prediction os text
-    :param y_true_geo: ground truth of geometry
-    :param y_pred_geo: prediction of geometry
-    :param training_mask: mask used in training, to ignore some text annotated by ###
-    :return:
-    '''
-    classification_loss = dice_coefficient(y_true_cls, y_pred_cls, training_mask)
-    # scale classification loss to match the iou loss part
-    classification_loss *= 0.01
+def compute_loss(binarize_map, threshold_map, thresh_binary,
+                 gt_score_maps, gt_threshold_map, gt_score_mask, gt_thresh_mask):
 
-    # d1 -> top, d2->right, d3->bottom, d4->left
-    d1_gt, d2_gt, d3_gt, d4_gt, theta_gt = tf.split(value=y_true_geo, num_or_size_splits=5, axis=3)
-    d1_pred, d2_pred, d3_pred, d4_pred, theta_pred = tf.split(value=y_pred_geo, num_or_size_splits=5, axis=3)
+    binarize_loss = dice_coefficient(gt_score_maps, binarize_map, gt_score_mask)
+    threshold_loss = smooth_l1_loss(threshold_map, gt_threshold_map, gt_thresh_mask)
+    thresh_binary_loss = dice_coefficient(gt_score_maps, thresh_binary, gt_score_mask)
+    pass
 
-    # 计算真实旋转框、预测旋转框面积
-    area_gt = (d1_gt + d3_gt) * (d2_gt + d4_gt)
-    area_pred = (d1_pred + d3_pred) * (d2_pred + d4_pred)
 
-    # 计算相交部分的高度和宽度  面积
-    w_union = tf.minimum(d2_gt, d2_pred) + tf.minimum(d4_gt, d4_pred)
-    h_union = tf.minimum(d1_gt, d1_pred) + tf.minimum(d3_gt, d3_pred)
 
-    # 计算R_true与R_pred的交集
-    area_intersect = w_union * h_union
-
-    # 计算R_true与R_pred的并集
-    area_union = area_gt + area_pred - area_intersect
-
-    # IoU loss,加1为了防止交集为0，log0没意义
-    L_AABB = -tf.log((area_intersect + 1.0) / (area_union + 1.0))
-
-    # 夹角的loss
-    L_theta = 1 - tf.cos(theta_pred - theta_gt)
-    # tf.summary.scalar('geometry_AABB', tf.reduce_mean(L_AABB * y_true_cls * training_mask))
-    # tf.summary.scalar('geometry_theta', tf.reduce_mean(L_theta * y_true_cls * training_mask))
-
-    valid_pts_nums = tf.cast(tf.count_nonzero(y_true_cls * training_mask), dtype=tf.float32)
-    tf.summary.scalar('geometry_AABB', tf.reduce_sum(L_AABB * y_true_cls * training_mask) / valid_pts_nums)
-    tf.summary.scalar('geometry_theta', tf.reduce_sum(L_theta * y_true_cls * training_mask) / valid_pts_nums)
-
-    L_g = L_AABB + 20 * L_theta  # geometry_map loss
-
-    return tf.reduce_mean(L_g * y_true_cls * training_mask) + classification_loss
