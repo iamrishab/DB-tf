41880f9de6c7e14c0da0183911839502553fd661
chizhanyuefeng <chizhanyuefeng@gmail,com>
2019-12-16 14:18:21 +0800

    update

--- db_config.py
+++ db_config.py
@@ -17,27 +17,20 @@ cfg.MIN_TEXT_SIZE = 1
 cfg.TRAIN = edict()
 cfg.TRAIN.VERSION = '1216'
 # 多gpu训练
-cfg.TRAIN.VIS_GPU = '1,2'
-# batchsize 大小
+cfg.TRAIN.VIS_GPU = '5,6'
 cfg.TRAIN.BATCH_SIZE_PER_GPU = 12
 cfg.TRAIN.LOSS_ALPHA = 1.0
 cfg.TRAIN.LOSS_BETA = 10.0
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~dataload & aug~~~~~~~~~~~~~~~~~~~~~~~~~~
-cfg.TRAIN.IMG_DIR = ''
-cfg.TRAIN.LABEL_DIR = ''
+cfg.TRAIN.IMG_DIR = '/hostpersistent/zzh/dataset/open_data/ctw1500/train/text_image'
+cfg.TRAIN.LABEL_DIR = '/hostpersistent/zzh/dataset/open_data/ctw1500/train/text_label_curve'
 
-# 图像尺寸
 cfg.TRAIN.IMG_SIZE = 736
-# 图像缩放
 cfg.TRAIN.IMG_SCALE = [0.5, 1, 1, 1, 1.5, 2.0]
-# crop
 cfg.TRAIN.IS_CROP = True
-# 线程数
 cfg.TRAIN.NUM_READERS = 20
-# 数据增强概率
 cfg.TRAIN.DATA_AUG_PROB = 0.0
-# 数据增强方式
 cfg.TRAIN.AUG_TOOL = ['GaussianBlur',
                 'AverageBlur',
                 'MedianBlur',
@@ -48,29 +41,21 @@ cfg.TRAIN.AUG_TOOL = ['GaussianBlur',
                       ]
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~save ckpt and log~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# 训练次数
 cfg.TRAIN.MAX_STEPS = 10000000
-# 保存ckpt的步数
 cfg.TRAIN.SAVE_CHECKPOINT_STEPS = 2000
-# tensorboard
 cfg.TRAIN.SAVE_SUMMARY_STEPS = 100
-# 保存ckpt最大数
 cfg.TRAIN.SAVE_MAX = 100
-# log存放地址
-cfg.TRAIN.TRAIN_LOGS = os.path.join('/hostpersistent/zzh/ikkyyu/train_data/', 'tf_logs')
-# ckpt存放地址
-cfg.TRAIN.CHECKPOINTS_OUTPUT_DIR = os.path.join('/hostpersistent/zzh/ikkyyu/train_data/', 'ckpt')
+cfg.TRAIN.TRAIN_LOGS = os.path.join('/hostpersistent/zzh/lab/DB-tf', 'tf_logs')
+cfg.TRAIN.CHECKPOINTS_OUTPUT_DIR = os.path.join('/hostpersistent/zzh/lab/DB-tf', 'ckpt')
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~restore and pretrain~~~~~~~~~~~~~~~~~~~~~
-cfg.TRAIN.RESTORE = True
-cfg.TRAIN.RESTORE_CKPT_PATH = os.path.join('/hostpersistent/zzh/ikkyyu/train_data/', 'ckpt')
-cfg.TRAIN.PRETRAINED_MODEL_PATH = '/hostpersistent/zzh/ikkyyu/east_models/V0.9.29'
-
+cfg.TRAIN.RESTORE = False
+cfg.TRAIN.RESTORE_CKPT_PATH = os.path.join('/hostpersistent/zzh/lab/DB-tf', 'ckpt')
+cfg.TRAIN.PRETRAINED_MODEL_PATH = '/hostpersistent/zzh/pretrain_models/resnet_v1_50.ckpt'
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~super em~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-cfg.TRAIN.AABB_LOSS = 1
 cfg.TRAIN.LEARNING_RATE = 0.0001
-cfg.TRAIN.OPT = 'momentum'#'adam'#
+cfg.TRAIN.OPT = 'adam'#'momentum'#
 cfg.TRAIN.MOVING_AVERAGE_DECAY = 0.997
 cfg.TRAIN.MIN_CROP_SIDE_RATIO = 0.00001
 
